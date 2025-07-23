87d8b5b5e20966a17c129115aeb7d6164137a760
DeepSeeDev <hyuk2000s@naver.com>
2025-07-23 16:57:34 +0900

    nav layout & todo layout

--- frontend/ch01/src/router/root.tsx
+++ frontend/ch01/src/router/root.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router";
 
 import {lazy, Suspense} from "react";
 import BasicLayout from "../layouts/basicLayout";
+import todoRouter from "./todoRouter";
 
 
 const Loading = () => <div>Loading...</div>
 
 const Main = lazy(() => import("../pages/mainPage"))
 const About = lazy(() => import("../pages/aboutPage"))
+
 
 const router = createBrowserRouter([
   {
@@ -22,6 +24,7 @@ const router = createBrowserRouter([
             path: 'about',
             element: <Suspense fallback={<Loading/>}><About/></Suspense>
         },
+        todoRouter()
     ]
   }
 ]);
