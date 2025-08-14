02876a24f245e3ac5df958f8218eaaaaf4ef4da3
DeepSeeDev <hyuk2000s@naver.com>
2025-08-14 18:10:08 +0900

    refreshToken 에러 수정

--- frontend/ch01/src/api/memberApi.tsx
+++ frontend/ch01/src/api/memberApi.tsx
@@ -1,18 +1,16 @@
 import axios from "axios"
 
 
-export const loginPost = async (email: string, pw: string) => {
+export const loginPost = async (email:string, pw:string) => {
+  
+  const header = {headers: {"Content-Type": "x-www-form-urlencoded"}}
+  const form = new FormData()
+  form.append('username', email)
+  form.append('password', pw)
+  
+  const res = await axios.post(`http://localhost:8080/api/member/login`, form, header)
+  //2초간 delay
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  return res.data
 
-    const header = {headers: {"Content-Type": "x-www-form-urlencoded"}}
-
-    const form = new FormData()
-
-    form.append('email', email)
-    form.append('pw', pw)
-
-    const res = await axios.post(`http://localhost:8080/api/member/login`, form, header)
-
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    return res.data
 }
